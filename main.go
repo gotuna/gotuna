b4b64afbb4c26540e9f1b367d354642b57e2d8dc
Milos Stojanovic <alcalbg@gmail.com>
2021-03-23 14:50:13 +0100

    cont

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,14 +12,39 @@ import (
 	"github.com/gorilla/sessions"
 )
 
+type MemoryUserRepository struct {
+	users []app.User
+}
+
+func NewMemoryUserRepository() app.UserRepository {
+	repo := &MemoryUserRepository{}
+	repo.users = []app.User{
+		app.User{SID: "1", Email: "alcalbg@gmail.com", PasswordHash: ""},
+	}
+	return repo
+}
+
+func (repo *MemoryUserRepository) GetUserByEmail(email string) (app.User, error) {
+	for _, user := range repo.users {
+		if user.Email == email {
+			return user, nil
+		}
+	}
+
+	return app.User{}, errors.New("user not found")
+
+}
+
 func main() {
 
 	port := ":8888"
 
-	logger := log.New(os.Stdout, "", 0)
-
 	cookieStore := sessions.NewCookieStore([]byte(os.Getenv("APP_KEY")))
-	srv := app.NewServer(logger, sessions.NewSession(cookieStore, session.SessionName))
+	srv := app.NewServer(
+		log.New(os.Stdout, "", 0),
+		sessions.NewSession(cookieStore, session.SessionName),
+		NewMemoryUserRepository(),
+	)
 
 	fmt.Printf("starting server at http://localhost%s \n", port)
 
