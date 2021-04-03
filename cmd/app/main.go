22befcbabb9abef881c6c4bfceb87aebdb748ce6
Milos Stojanovic <alcalbg@gmail.com>
2021-04-03 14:46:42 +0200

    fix main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -7,17 +7,23 @@ import (
 	"os"
 
 	"github.com/alcalbg/gotdd"
+	"github.com/alcalbg/gotdd/static"
 	"github.com/alcalbg/gotdd/test/doubles"
+	"github.com/gorilla/sessions"
 )
 
 func main() {
 
 	port := ":8888"
+	keyPairs := os.Getenv("APP_KEY")
 
-	app := gotdd.App{
-		UserRepository: doubles.NewUserRepositoryStub(),
+	app := gotdd.NewApp(gotdd.App{
 		Logger:         log.New(os.Stdout, "", 0),
-	}
+		UserRepository: doubles.NewUserRepositoryStub(),
+		Session:        gotdd.NewSession(sessions.NewCookieStore([]byte(keyPairs))),
+		FS:             static.EmbededStatic,
+		StaticPrefix:   "",
+	})
 
 	fmt.Printf("starting server at http://localhost%s \n", port)
 
