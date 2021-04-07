c8f23f01430512260f6bed79d827df3008cfe393
Milos Stojanovic <alcalbg@gmail.com>
2021-04-07 11:19:43 +0200

    cont

--- context.go
+++ context.go
@@ -5,16 +5,16 @@ import (
 	"errors"
 )
 
-const userCtxKey = "user"
+const ctxKeyUser = "user"
 
 func WithUser(ctx context.Context, user User) context.Context {
-	return context.WithValue(ctx, userCtxKey, user)
+	return context.WithValue(ctx, ctxKeyUser, user)
 }
 
 func GetUser(ctx context.Context) (User, error) {
-	user, ok := ctx.Value(userCtxKey).(User)
+	user, ok := ctx.Value(ctxKeyUser).(User)
 	if !ok {
-		return nil, errors.New("no user in context")
+		return nil, errors.New("no user in the context")
 	}
 	return user, nil
 }
