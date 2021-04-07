1d3edca9e27c7f46612fbfffecbf819855bb596e
Milos Stojanovic <alcalbg@gmail.com>
2021-04-07 11:36:19 +0200

    cont

--- context_test.go
+++ context_test.go
@@ -12,8 +12,9 @@ import (
 func TestStoringAndGettingUserFromContext(t *testing.T) {
 
 	t.Run("empty context doesnt have a user", func(t *testing.T) {
-		_, err := gotdd.GetUser(context.Background())
+		user, err := gotdd.GetUser(context.Background())
 		assert.Error(t, err)
+		assert.Equal(t, nil, user)
 	})
 
 	t.Run("store and retrieve fake user", func(t *testing.T) {
