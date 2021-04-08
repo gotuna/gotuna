742669bef1659fe1cfcc613f822f7e18d134e736
Milos Stojanovic <alcalbg@gmail.com>
2021-04-08 10:13:19 +0200

    cont

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -19,6 +19,27 @@ import (
 	"github.com/gorilla/sessions"
 )
 
+var User1 = gotdd.InMemoryUser{
+	UniqueID: "123",
+	Email:    "john@example.com",
+	Name:     "John",
+	Password: "pass123",
+}
+
+var User2 = gotdd.InMemoryUser{
+	UniqueID: "456",
+	Email:    "bob@example.com",
+	Name:     "Bob",
+	Password: "bobby5",
+}
+
+func NewUserRepository() gotdd.UserRepository {
+	return gotdd.NewInMemoryUserRepository([]gotdd.InMemoryUser{
+		User1,
+		User2,
+	})
+}
+
 func main() {
 
 	port := ":8888"
